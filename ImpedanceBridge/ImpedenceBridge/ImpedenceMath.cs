32fc040fb3d215ade2772f5281523cbdb6eee654
Andreya Grzegorzewski <a-grzegorzewski@onu.edu>
2016-11-09 18:28:08 -0500

    Added ImpedanceMathImp class

---
+++ ImpedanceBridge/ImpedenceBridge/ImpedenceMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpedanceBridge
+{
+    interface ImpedanceMath // This is the Abstraction interface
+    {
+        ComplexNumber getParallelImpedance(ComplexNumber num1, ComplexNumber num2);
+        ComplexNumber getSeriesImpedance(ComplexNumber num1, ComplexNumber num2);
+    }
+}
